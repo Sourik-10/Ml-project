5415312d11d5a9bd1860b531eadca10c2e9a00a6
Sourik <ghoshsourik.48@gmail.com>
2026-05-12 01:40:19 +0530

    data trasformation

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from src.ml_project.logger import logging
 from src.ml_project.exception import CustomException
 import sys
+
 from src.ml_project.components.data_ingestion import DataIngestion
-from src.ml_project.components.data_ingestion import DataIngestionConfig
+from src.ml_project.components.data_transformation import DataTransformation
 
 
 if __name__ == "__main__":
+
     logging.info("This is a log message from app.py")
+
     try:
-      #data_ingestion_config = DataIngestionConfig()
-      data_ingestion = DataIngestion()
-      data_ingestion.initiate_data_ingestion()
-      
+
+        data_ingestion = DataIngestion()
+
+        train_data_path, test_data_path = (
+            data_ingestion.initiate_data_ingestion()
+        )
+
+        data_transformation = DataTransformation()
+
+        train_arr, test_arr, _ = (
+            data_transformation.initiate_data_transformation(
+                train_data_path,
+                test_data_path
+            )
+        )
+
+        logging.info("Data Transformation completed")
+
     except Exception as e:
-      logging.info("An error occurred in app.py")
-      raise CustomException(e, sys)     
-    
+
+        logging.info("An error occurred in app.py")
+
+        raise CustomException(e, sys)
